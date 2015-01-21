59bcefcc04528a4c52b39a90ab33a48bcfca4dc6
Alex Wissmann <awiss@wharton.upenn.edu>
2015-01-20 22:24:58 -0500

    using csv module

--- buildingRedis.py
+++ buildingRedis.py
@@ -1,14 +1,9 @@
 import redis
 import json
-
+import csv
 
 db = redis.StrictRedis(host='localhost', port=6379, db=0)
 with open('buildings.csv') as f:
-  for line in f:
-    arr = line.split(",")
-    building = dict()
-    building["name"] = arr[0]
-    building["latitude"] = arr[1]
-    building["longitude"] = arr[2]
-    print "setting %s to %s" % (arr[4], json.dumps(building))
-    db.set("buildings:%s" % (arr[4]), json.dumps(building))
+  reader = csv.DictReader(f)
+  for row in reader:
+    db.set("buildings:%s" % (row["code_courses"]), json.dumps(row))
