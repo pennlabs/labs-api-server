67d1b4b526b1970e0459c41ae1e0ba79466b0b6f
Alex Wissmann <awiss@wharton.upenn.edu>
2015-02-10 22:58:14 -0500

    Fixed tabbing issue, added stop locations for transit

--- server/penndata.py
+++ server/penndata.py
@@ -1,7 +1,8 @@
-from penn import directory, dining, registrar, Map
+from penn import Transit, Directory, Dining, Registrar, Map
 from os import getenv
 
-din = dining.Dining(getenv("DIN_USERNAME"), getenv("DIN_PASSWORD"))
-reg = registrar.Registrar(getenv("REG_USERNAME"), getenv("REG_PASSWORD"))
-penn_dir = directory.Directory(getenv("DIR_USERNAME"), getenv("DIR_PASSWORD"))
+din = Dining(getenv("DIN_USERNAME"), getenv("DIN_PASSWORD"))
+reg = Registrar(getenv("REG_USERNAME"), getenv("REG_PASSWORD"))
+penn_dir = Directory(getenv("DIR_USERNAME"), getenv("DIR_PASSWORD"))
 map_search = Map(getenv("NEM_USERNAME"), getenv("NEM_PASSWORD"))
+transit = Transit(getenv("TRANSIT_USERNAME"), getenv("TRANSIT_PASSWORD"))
