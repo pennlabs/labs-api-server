6f88ff7c4cc1720bb15b47583b4ba9aafb090e85
Adel Qalieh <aqalieh95@gmail.com>
2015-11-12 11:52:47 -0500

    Handle laundry exceptions from Python SDK

--- server/laundry.py
+++ server/laundry.py
@@ -1,11 +1,20 @@
 from flask import jsonify
 from server import app
 from penndata import *
+from requests.exceptions import HTTPError
+
 
 @app.route('/laundry/halls', methods=['GET'])
 def all_halls():
-  return jsonify({"halls": laundry.all_status()})
+    try:
+        return jsonify({"halls": laundry.all_status()})
+    except HTTPError:
+        return jsonify({'error': 'The laundry api is currently unavailable.'})
+
 
 @app.route('/laundry/hall/<hall_no>', methods=['GET'])
 def hall(hall_no):
-  return jsonify(laundry.hall_status(int(hall_no)))
+    try:
+        return jsonify(laundry.hall_status(int(hall_no)))
+    except HTTPError:
+        return jsonify({'error': 'The laundry api is currently unavailable.'})
