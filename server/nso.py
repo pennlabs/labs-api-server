ed0f115e600a564117ed540e7692e0efccf5826b
Adam Domingoes <adamdom@wharton.upenn.edu>
2016-08-24 21:36:48 -0400

    Set time back four hours to EST

--- server/nso.py
+++ server/nso.py
@@ -11,6 +11,22 @@ sys.setdefaultencoding('utf-8')
 def get_nso_events():
     r = requests.get("http://www.nso.upenn.edu/event-calendar.rss")
     split = r.text.split("\n")
-    filtered = [i if  "<pubDate" not in i else "<pubDate>Wed, 02 Aug 2016 08:00:00 EST</pubDate>" for i in split]
+    filtered = [i if "<pubDate" not in i else "<pubDate>Wed, 02 Aug 2016 08:00:00 EST</pubDate>" for i in split]
+    filtered = [i if ("<title" not in i or "NSO Event Calendar" in i) else changeTitle(i) for i in filtered]
     output = "\n".join(filtered)
     return Response(output, mimetype="text/xml")
+
+def changeTitle(a):
+    index = a.index("event") + 17
+    a = subFour(a,index)
+    if a[index+6] == '-':
+        a = subFour(a,index + 18)
+    return a
+
+def subFour(string, index):
+    val = string[index:index+6]
+    new_val = str(int(val) - 40000)
+    if len(new_val) < 6:
+        new_val = "0" + new_val
+    return string.replace(val, new_val)
+
