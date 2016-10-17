fdd550cd2688d1c53d5a94636e79060ebe2b49d9
Adel Qalieh <aqalieh95@gmail.com>
2016-10-17 11:44:50 -0400

    PEP8 NSO server file

--- server/nso.py
+++ server/nso.py
@@ -1,41 +1,42 @@
-from flask import request, Response
-from .base import *
+from flask import Response
 from server import app
 import requests
-import re
 import sys
 import datetime
-reload(sys)
 sys.setdefaultencoding('utf-8')
+
 
 @app.route('/nso')
 def get_nso_events():
     r = requests.get("http://www.nso.upenn.edu/event-calendar.rss")
     split = r.text.split("\n")
+    # TODO: Rework into for-loop
     filtered = [i if "<pubDate" not in i else "<pubDate>" + parseDate(i) + " EST</pubDate>" for i in split]
     filtered = [i if ("<title" not in i or "NSO Event Calendar" in i) else changeTitle(i) for i in filtered]
     output = "\n".join(filtered)
     return Response(output, mimetype="text/xml")
 
+
 def parseDate(s):
     split = s.split("pubDate")
     s = split[1][1:-8]
-    d = datetime.datetime.strptime(s,"%Y-%m-%dT%H:%M:%S")
+    d = datetime.datetime.strptime(s, "%Y-%m-%dT%H:%M:%S")
     return d.strftime("%a, %d %b %Y %X %Z")
+
 
 def changeTitle(a):
     index = a.index("event") + 17
-    a = subFour(a,index)
+    a = subFour(a, index)
     if a[index+6] == '-':
-        a = subFour(a,index + 18)
+        a = subFour(a, index + 18)
     return a
+
 
 def subFour(string, index):
     val = string[index:index+6]
-    new_val = str((int(val) - 40000 + 240000)%240000)
+    new_val = str((int(val) - 40000 + 240000) % 240000)
     if len(new_val) < 6:
         new_val = "0" + new_val
     if len(new_val) == 2:
         new_val = "000000"
     return string.replace(val, new_val)
-
