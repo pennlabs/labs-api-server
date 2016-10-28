85945f53f372ed5498d0ee7b00afb7bf3ef2a36e
esqu1 <brandbest1@gmail.com>
2016-10-28 19:47:22 -0400

    fixed the query string

--- server/studyspaces.py
+++ server/studyspaces.py
@@ -6,35 +6,34 @@ from datetime import time
 import urllib2
 
 groupStudyCodes = {
-	"Biomedical Library - Group Study Rooms" : 505,
-	"Dental Library - Group Study Rooms" : 13107,
-	"Dental Library - Seminar Room" : 13532,
-	"Education Commons" : 848,
-	"Glossberg Record Room" : 1819,
-	"Levin Building Group Study Rooms" : 13489,
-	"Lippincott Library" : 1768,
-	"Lippincott Library Seminar Rooms" : 2587,
-	"Noldus Observer" : 3621,
-	"Van Pelt-Dietrich Library Center Group Study Rooms" : 1799,
-	"Van Pelt-Dietrich Library Center Seminar Rooms" : 4409,
-	"Weigle Information Commons" : 1722
+	505 : "Biomedical Library - Group Study Rooms",
+	13107 : "Dental Library - Group Study Rooms",
+	13532 : "Dental Library - Seminar Room",
+	848 : "Education Commons",
+	1819 : "Glossberg Record Room",
+	13489 : "Levin Building Group Study Rooms",
+	1768 : "Lippincott Library",
+	2587 : "Lippincott Library Seminar Rooms",
+	3621 : "Noldus Observer",
+	1799 : "Van Pelt-Dietrich Library Center Group Study Rooms",
+	4409 : "Van Pelt-Dietrich Library Center Seminar Rooms",
+	1722 : "Weigle Information Commons"
 } # this should be moved somewhere else
 
 @app.route('/studyspaces/<date>', methods=['GET']) # id is given by dictionary above
 def parseTimes(date): # Returns JSON with available rooms
-	id = request.args['id']
-	print request.args
-	if(id != None):
+	if 'id' in request.args:
+		id = request.args['id']
 		return jsonify({'studyspaces' : extractTimes(id,date)})
 	else:
 		l = []
-		for key in groupStudyCodes:
-			l += extractTimes(groupStudyCodes[key],date)
+		for id in groupStudyCodes:
+			l += extractTimes(id,date)
 		return jsonify({'studyspaces' : l})
 
 
 def extractTimes(id,date):
-	url = "http://libcal.library.upenn.edu/rooms_acc.php?gid=%s&d=%s&cap=0" % (id,date)
+	url = "http://libcal.library.upenn.edu/rooms_acc.php?gid=%s&d=%s&cap=0" % (int(id),date)
 	soup = BeautifulSoup(urllib2.urlopen(url).read(), 'lxml')
 
 	timeSlots = soup.find_all('form')
@@ -51,7 +50,9 @@ def extractTimes(id,date):
 		newRoom = str(roomName)[:-1]
 		times = []
 
-		for t in room.fieldset.find_all('label'):
+		filt = room.fieldset.find_all('label')
+
+		for t in filt: # getting the individual times for each room 
 			dictItem = {}
 			dictItem['room_name'] = newRoom
 			time = str(t).split("\t\t\t\t\t")[1][1:-1]
@@ -61,10 +62,10 @@ def extractTimes(id,date):
 			dictItem['end_time'] = startAndEnd[1].upper()
 			roomTimes.append(dictItem)
 			dictItem['date'] = dateParse(date)
+			dictItem['building'] = groupStudyCodes[int(id)]
+	return roomTimes
 
-	return {'studyspaces' : roomTimes}
-
-def dateParse(d):
+def dateParse(d): # parses the date into a better format
 	l = d.split("-")
 	final = [l[1],l[2],l[0]]
 	return '-'.join(final)
