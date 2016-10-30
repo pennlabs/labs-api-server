6981570908a0d2ebb06868904373aff75a66d2f3
esqu1 <brandbest1@gmail.com>
2016-10-29 21:51:19 -0400

    caching started

--- server/weather.py
+++ server/weather.py
@@ -1,24 +1,36 @@
 from flask import request, jsonify
-from .base import *
 from server import app
-import urllib2
+import requests
 import datetime
+
 
 @app.route('/weather', methods=['GET'])
 def retrieve():
-	try:
-		f = open('.cache','r+w')
-	except IOError:
-		f = open('.cache','w')
-		f.close()
-		f = open('.cache','r+w')
-	g = open('.app.id','r')
-	APPID = g.read()
-	g.close()
-	d = datetime.datetime.now()
-	url = 'http://api.openweathermap.org/data/2.5/weather?q=Philadelphia&units=imperial&APPID=%s' % APPID
-	json = urllib2.urlopen(url).read()
-	time = d.strftime('%m %d %Y %H %M %S')
-	f.write(time + "\n\n" + json)
-	f.close()
-	return json
+    try:
+        f = open('.cache', 'r+w')
+    except IOError:
+        # if file does not exist
+        f = open('.cache', 'w')
+        f.close()
+        f = open('.cache', 'r+w')
+    g = open('.app.id', 'r')
+    APPID = g.read()
+    g.close()
+
+    contents = f.read()
+    if contents != '':
+        d = datetime.datetime.now()
+        cache = contents.split("\n")
+        time_stamp = cache[0]
+        cached_date = datetime.datetime.strptime(time_stamp, "%m %d %Y %H %M %S")
+        delta = abs(cached_date - d)
+        if delta.seconds >= 600:
+            url = 'http://api.openweathermap.org/data/2.5/weather?q=Philadelphia&units=imperial&APPID=%s' % APPID
+            json = requests.get(url).json()
+            time = d.strftime('%m %d %Y %H %M %S')
+            f.write(time + "\n" + json)
+            f.close()
+            return json
+        else:
+            f.close()
+            return cache[1]
