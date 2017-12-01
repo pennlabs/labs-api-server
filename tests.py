94021dc039957819250189023ef76ba6bcfbfabe
Eric Wang <ezwang1@gmail.com>
2017-11-30 23:25:15 -0500

    add test for database

--- tests.py
+++ tests.py
@@ -4,6 +4,8 @@ import server
 import json
 import datetime
 
+from server.models import sqldb, LaundrySnapshot
+
 ## Fake
 authHeaders = [(
     'cookie',
@@ -15,6 +17,33 @@ AUTH_TOKEN = b'5e625cf41e3b7838c79b49d890a203c568a44c3b27362b0a06ab6f08bec8f677'
 class MobileAppApiTests(unittest.TestCase):
     def setUp(self):
         server.app.config['TESTING'] = True
+
+    @classmethod
+    def setUpClass(self):
+        with server.app.test_request_context():
+            for x in range(0, 24*60, 60):
+                item = LaundrySnapshot(
+                    date=datetime.date(2017, 1, 1),
+                    time=x,
+                    room=1,
+                    washers=3,
+                    dryers=3,
+                    total_washers=3,
+                    total_dryers=3
+                )
+                sqldb.session.add(item)
+            for x in range(0, 24*60, 60):
+                item = LaundrySnapshot(
+                    date=datetime.date(2017, 1, 1) - datetime.timedelta(days=7),
+                    time=x,
+                    room=1,
+                    washers=0,
+                    dryers=0,
+                    total_washers=3,
+                    total_dryers=3
+                )
+                sqldb.session.add(item)
+            sqldb.session.commit()
 
     def testDiningVenues(self):
         with server.app.test_request_context():
@@ -153,6 +182,16 @@ class MobileAppApiTests(unittest.TestCase):
             self.assertEquals(res['end_date'], '2017-01-01')
             self.assertEquals(len(res['washer_data']), 27)
             self.assertEquals(len(res['dryer_data']), 27)
+
+    def testLaundryDatabase(self):
+        with server.app.test_request_context():
+            request = server.laundry.usage(1, 2017, 1, 1)
+            res = json.loads(request.data.decode('utf8'))
+            self.assertEquals(res['total_number_of_washers'], 3)
+            self.assertEquals(res['total_number_of_dryers'], 3)
+            for x in range(0, 23):
+                self.assertEquals(res['washer_data'][str(x)], 1.5)
+                self.assertEquals(res['dryer_data'][str(x)], 1.5)
 
     def testStudyspacesIDs(self):
         with server.app.test_request_context():
