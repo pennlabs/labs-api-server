4f8ecb0fc51ab03f4192c07b46bad8f6c3095d6d
Eric Wang <ezwang1@gmail.com>
2018-01-26 00:15:25 -0500

    error handling

--- server/studyspaces.py
+++ server/studyspaces.py
@@ -16,19 +16,23 @@ def parse_times(building):
         /studyspaces/availability/<building>?start=...&end=... gives all rooms between the two times
     """
     date = datetime.datetime.now()
-    start = request.args.get('start')
-    if start is not None:
-        start = datetime.datetime.strptime(start, "%Y-%m-%dT%H:%M:%S%z")
-    else:
-        start = date
-    end = request.args.get('end')
-    if end is not None:
-        end = datetime.datetime.strptime(end, "%Y-%m-%dT%H:%M:%S%z")
-    else:
-        end = start + datetime.timedelta(days=1)
+    try:
+        start = request.args.get('start')
+        if start is not None:
+            start = datetime.datetime.strptime(start, "%Y-%m-%dT%H:%M:%S%z")
+        else:
+            start = date
+        end = request.args.get('end')
+        if end is not None:
+            end = datetime.datetime.strptime(end, "%Y-%m-%dT%H:%M:%S%z")
+        else:
+            end = start + datetime.timedelta(days=1)
+    except ValueError:
+        return jsonify({"error": "Incorrect date format!"})
+
     return jsonify({
         "location_id": building,
-        "date": date.strftime("%Y-%m-%d"),
+        "date": start.strftime("%Y-%m-%d"),
         "rooms": studyspaces.get_rooms(building, start, end)
     })
 
