4879ce3811e716030409eb781bf9e65922424912
Eric Wang <ezwang1@gmail.com>
2018-01-25 23:04:00 -0500

    make start and end times configurable

--- server/studyspaces.py
+++ server/studyspaces.py
@@ -1,6 +1,6 @@
 import datetime
 
-from flask import jsonify
+from flask import jsonify, request
 from server import app
 from .penndata import studyspaces
 
@@ -14,10 +14,20 @@ def parse_times(building):
         /studyspaces/<building> gives all rooms for today
     """
     date = datetime.date.today()
+    start = request.args.get('start')
+    if start is not None:
+        start = datetime.datetime.strptime(start, "%Y-%m-%d").date()
+    else:
+        start = date
+    end = request.args.get('end')
+    if end is not None:
+        end = datetime.datetime.strptime(end, "%Y-%m-%d").date()
+    else:
+        end = start + datetime.timedelta(days=1)
     return jsonify({
         "location_id": building,
         "date": date.strftime("%Y-%m-%d"),
-        "rooms": studyspaces.get_rooms(building, date, date + datetime.timedelta(days=1))
+        "rooms": studyspaces.get_rooms(building, start, end)
     })
 
 
