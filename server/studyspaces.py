91a6074a270ee90f1881497bfd8a1f1bcba7ae42
Eric Wang <ezwang1@gmail.com>
2018-01-25 23:41:53 -0500

    modify endpoints

--- server/studyspaces.py
+++ server/studyspaces.py
@@ -5,15 +5,15 @@ from server import app
 from .penndata import studyspaces
 
 
-@app.route('/studyspaces/<int:building>', methods=['GET'])
+@app.route('/studyspaces/availability/<int:building>', methods=['GET'])
 def parse_times(building):
     """
     Returns JSON containing all rooms for a given building.
 
     Usage:
-        /studyspaces/<building> gives all rooms for the next 24 hours
-        /studyspaces/<building>?start=2018-25-01T11:00:00-0500 gives all rooms from start to 24 hours later
-        /studyspaces/<building>?start=...&end=... gives all rooms between the two times
+        /studyspaces/availability/<building> gives all rooms for the next 24 hours
+        /studyspaces/availability/<building>?start=2018-25-01T11:00:00-0500 gives all rooms from start to 24 hours later
+        /studyspaces/availability/<building>?start=...&end=... gives all rooms between the two times
     """
     date = datetime.datetime.now()
     start = request.args.get('start')
@@ -33,7 +33,7 @@ def parse_times(building):
     })
 
 
-@app.route('/studyspaces/', methods=['GET'])
+@app.route('/studyspaces/locations', methods=['GET'])
 def display_id_pairs():
     """
     Returns JSON containing a list of buildings with their ids.
