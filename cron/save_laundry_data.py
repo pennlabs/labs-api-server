2c71e92b47b8ccaa4ef23e9c17b87c356f1d71a0
Eric Wang <ezwang1@gmail.com>
2018-01-27 14:57:52 -0500

    document crontab

--- cron/save_laundry_data.py
+++ cron/save_laundry_data.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+# Add the following line into the labs crontab.
+# */15 * * * * /home/labs/penn-mobile-server/cron/save_laundry_data.py
+
 import server
 
 server.laundry.save_data()
