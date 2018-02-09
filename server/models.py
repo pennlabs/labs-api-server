ae8cbfbc12b28f6b60f1cb89ebf5e9be96217a20
Eric Wang <ezwang1@gmail.com>
2018-02-08 23:00:00 -0500

    add study space booking model

--- server/models.py
+++ server/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from flask_sqlalchemy import SQLAlchemy
 
 sqldb = SQLAlchemy()
@@ -12,3 +14,13 @@ class LaundrySnapshot(sqldb.Model):
     dryers = sqldb.Column(sqldb.Integer, nullable=False)
     total_washers = sqldb.Column(sqldb.Integer, nullable=False)
     total_dryers = sqldb.Column(sqldb.Integer, nullable=False)
+
+
+class StudySpacesBooking(sqldb.Model):
+    id = sqldb.Column(sqldb.Integer, primary_key=True)
+    date = sqldb.Column(sqldb.DateTime, default=datetime.datetime.utcnow)
+    lid = sqldb.Column(sqldb.Integer, nullable=False)
+    rid = sqldb.Column(sqldb.Integer, nullable=False)
+    email = sqldb.Column(sqldb.Text, nullable=False)
+    start = sqldb.Column(sqldb.DateTime, nullable=False)
+    end = sqldb.Column(sqldb.DateTime, nullable=False)
