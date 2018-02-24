6d22e8ada8facb0edaf8f760f3f96c821960ae41
Adam Ripley <aripley@wharton.upenn.edu>
2018-02-24 15:35:45 -0500

    check for valid request values

--- server/homepage.py
+++ server/homepage.py
@@ -15,12 +15,24 @@ def get_homepage():
         "cells": cells
     })
 
+# Error check request cell options
+def error_options(options):
+    with open('homepage_options.json') as json_file:
+        data = json.load(json_file)
+    for option in options:
+        if (option not in data['cellOptions']):
+            print('not', option)
+            return True
+    return False
+
 @app.route('/homepage', methods=['POST'])
 def change_order():
     cellOptions = request.get_json()
     authSecret = getenv('AUTH_SECRET')
     if (authSecret != request.headers.get('authSecret')):
         return "Please provide proper auth token with request."
+    elif error_options(cellOptions['cellOptions']):
+        return "Not all cell options are valid."
     else:
         with open('homepage_options.json', 'w') as outfile:
             json.dump(cellOptions, outfile)
