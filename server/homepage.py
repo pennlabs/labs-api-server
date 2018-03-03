1024ccc9d3ea7051c8e0bf7abd40182b0abed677
Josh <jsdoman97@gmail.com>
2018-03-03 15:50:09 -0500

    create HomeCell model, added info to cells

--- server/homepage.py
+++ server/homepage.py
@@ -1,7 +1,7 @@
 from flask import request, jsonify
 from server import app, sqldb
 from os import getenv
-from .models import User, DiningPreference
+from .models import User, DiningPreference, LaundryPreference, HomeCell
 from sqlalchemy import func
 import json
 
@@ -17,17 +17,54 @@ def get_homepage():
         print(e)
         return jsonify({'err': ['error']})
 
-    preferences = sqldb.session.query(DiningPreference.venue_id, func.count(DiningPreference.venue_id)) \
-                               .filter_by(user_id=user.id).group_by(DiningPreference.venue_id).all()
-    preference_arr = [x[0] for x in preferences]
+    cell = get_popular_dining_cell(user)
+
     # Display information
-    cells = [{"type": x, "info": ""} for x in data['cellOptions']]
-    for x in cells:
-        if x["type"] == 'dining':
-            x["info"] = {'visited_halls': preference_arr}
-    return jsonify({
-        "cells": cells
-    })
+    # cells = [{"type": x, "info": ""} for x in data['cellOptions']]
+    cells = []
+    diningCell = get_popular_dining_cell(user).getCell()
+    cells.append(diningCell)
+
+    laundryCells = [x.getCell() for x in get_laundry_cells(user)]
+    cells.extend(laundryCells)
+
+    gsrCell = get_study_spaces_cell().getCell()
+    cells.append(gsrCell)
+
+    response = jsonify({"cells": cells})
+    response.status_code = 200 # or 400 or whatever
+    return response
+
+# returns a dining cell containing the users preferences in sorted order
+def get_dining_preference_cell(user):
+    preferences = sqldb.session.query(DiningPreference.venue_id) \
+                               .filter_by(user_id=user.id).group_by(DiningPreference.venue_id) \
+                               .order_by(func.count(DiningPreference.venue_id).desc()).all()
+    venue_ids = [x.venue_id for x in preferences]
+    return HomeCell("dining", info)
+
+# returns a dining cell
+# TODO: personalize with preferences
+def get_popular_dining_cell(user):
+    venue_ids = [593, 747, 636, 1442]
+    info = {"venues": venue_ids}
+    cell = HomeCell("dining", info)
+    return cell
+
+# returns a list of laundry cells
+def get_laundry_cells(user):
+    preferences = LaundryPreference.query.filter_by(user_id=user.id)
+    room_ids = [x.room_id for x in preferences]
+
+    # If the user has no preferences, select Bishop White
+    if len(room_ids) == 0:
+        room_ids.append(0)
+
+    return [HomeCell("laundry", { "room_id": x }) for x in room_ids]
+
+# returns a study spaces cell
+def get_study_spaces_cell():
+    return HomeCell("studyRoomBooking", None)
 
 # Error check request cell options
 def error_options(options):
