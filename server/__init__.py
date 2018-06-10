60e988c31d5df4b3139fc0089506cc25aca916a7
Eric Wang <ezwang1@gmail.com>
2018-06-10 14:13:08 -0400

    remove old/broken categories

--- server/__init__.py
+++ server/__init__.py
@@ -26,15 +26,12 @@ db = redis.StrictRedis(host='localhost', port=6379, db=0)
 app.secret_key = os.urandom(24)
 
 import server.registrar
-import server.transit
 import server.dining
-import server.buildings
 import server.directory
 import server.laundry
 import server.auth
 import server.pcr
 import server.athletics
-import server.nso
 import server.studyspaces
 import server.weather
 import server.calendar3year
