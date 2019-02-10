4580ddd7fdfe39a05d7914a55301eca536bdb771
Josh <jsdoman97@gmail.com>
2019-02-10 11:10:01 -0500

    removed old unused routes

--- server/dining.py
+++ server/dining.py
@@ -91,25 +91,6 @@ def retrieve_daily_menu(venue_id):
 
 @app.route('/dining/preferences', methods=['POST'])
 def save_dining_preferences():
-    # try:
-    #     user = User.get_or_create()
-    # except ValueError as e:
-    #     return jsonify({"success": False, "error": str(e)})
-
-    # venue_id = request.form.get('venue_id')
-
-    # if not venue_id:
-    #     return jsonify({'success': False, 'error': 'No venue specified.'})
-
-    # dining_preference = DiningPreference(user_id=user.id, venue_id=venue_id)
-    # sqldb.session.add(dining_preference)
-    # sqldb.session.commit()
-
-    return jsonify({'success': True, 'error': None})
-
-
-@app.route('/dining/preferences/V2', methods=['POST'])
-def save_dining_preferences_v2():
     try:
         user = User.get_or_create()
     except ValueError as e:
@@ -132,16 +113,3 @@ def save_dining_preferences_v2():
     sqldb.session.commit()
 
     return jsonify({'success': True, 'error': None})
-
-
-@app.route('/dining/preferences', methods=['GET'])
-def get_dining_preferences():
-    try:
-        user = User.get_or_create()
-    except ValueError:
-        return jsonify({'preferences': []})
-
-    preferences = sqldb.session.query(DiningPreference.venue_id, func.count(DiningPreference.venue_id)) \
-                               .filter_by(user_id=user.id).group_by(DiningPreference.venue_id).all()
-    preference_arr = [{'venue_id': x[0], 'count': x[1]} for x in preferences]
-    return jsonify({'preferences': preference_arr})
