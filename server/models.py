10aaa8ce4054321140c5bbdea4b846ea3d319c25
Josh <jsdoman97@gmail.com>
2019-03-19 11:39:13 -0400

    fixed start_time field

--- server/models.py
+++ server/models.py
@@ -76,7 +76,7 @@ class Course(sqldb.Model):
 class CourseMeetingTime(sqldb.Model):
     course_id = sqldb.Column(sqldb.Integer, sqldb.ForeignKey("course.id"), primary_key=True)
     weekday = sqldb.Column(sqldb.VARCHAR(3), primary_key=True)
-    start_time = sqldb.Column(sqldb.Text, primary_key=True)
+    start_time = sqldb.Column(sqldb.VARCHAR(10), primary_key=True)
     end_time = sqldb.Column(sqldb.Text, nullable=False)
     building = sqldb.Column(sqldb.Text, nullable=True)
     room = sqldb.Column(sqldb.Text, nullable=True)
