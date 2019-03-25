095427744b3977b1ad4edc1d7cfe497ac7f2358f
Josh <jsdoman97@gmail.com>
2019-03-25 12:53:52 -0400

    fixed calendar end date

--- server/calendar3year.py
+++ server/calendar3year.py
@@ -3,6 +3,7 @@ from .penndata import calendar
 from .base import cache_get
 from server import app
 import datetime
+import re
 
 
 def pull_calendar(d):
@@ -18,8 +19,9 @@ def pull_calendar(d):
         event_date = datetime.datetime.strptime(start, '%Y-%m-%d').date()
         time_diff = event_date - d
         if time_diff.total_seconds() > 0 and time_diff.total_seconds() <= 1209600:
-            modified_event_date = event_date - datetime.timedelta(days=1)
-            event['end'] = datetime.datetime.strftime(modified_event_date, '%Y-%m-%d')
+            event['name'] = re.split('Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday', event['name'])[0].strip()
+            if "Advance Registration" in event['name']:
+                event['name'] = 'Advance Registration'
             within_range.append(event)
     return within_range
 
