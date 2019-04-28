dd95fb2ef9e87301cd2c3acf92bd65566f4773a8
Eric Wang <ezwang1@gmail.com>
2019-04-28 11:12:59 -0400

    add a way to search by instructor

--- server/registrar.py
+++ server/registrar.py
@@ -96,9 +96,25 @@ def search():
     def get_data():
         query_results = search_course(get_type_search(search_query))
         if query_results is None:
-            return {"error": "The search query could not be processed"}
+            return {"error": "The search query could not be processed."}
         else:
             return query_results
 
     return cached_route('registrar_query:%s' % search_query, datetime.timedelta(days=1),
                         get_data)
+
+
+@app.route('/registrar/search/instructor', methods=['GET'])
+def search_instructor():
+    query = request.args['q']
+
+    def get_data():
+        results = reg.search({
+            'instructor': query
+        })
+        if results is None:
+            return {"error": "The search query could not be processed."}
+        else:
+            return {"courses": list(results)}
+
+    return cached_route('registrar_query_instructor:%s' % query, datetime.timedelta(days=1), get_data)
