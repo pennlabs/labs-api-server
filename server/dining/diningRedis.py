ce2ac694cbd836e339f401b0119265ed927c2163
Eric Wang <ezwang1@gmail.com>
2019-09-17 18:24:17 -0400

    use correct relative path

--- server/dining/diningRedis.py
+++ server/dining/diningRedis.py
@@ -1,9 +1,10 @@
 import redis
 import json
 import csv
+import os
 
 db = redis.StrictRedis(host='localhost', port=6379, db=0)
-with open('server/dining/diningImages.csv') as f:
+with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'diningImages.csv')) as f:
     reader = csv.DictReader(f)
     for row in reader:
         db.set("venue:%s" % (row["id"]), json.dumps(row["imageUrl"]))
