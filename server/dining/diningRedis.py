1e31daf3b4c55d148fb980aa480fb9ef9b0edb16
Josh <jsdoman97@gmail.com>
2019-09-17 19:19:46 -0400

    commented out redis

--- server/dining/diningRedis.py
+++ server/dining/diningRedis.py
@@ -3,8 +3,8 @@ import json
 import csv
 import os
 
-db = redis.StrictRedis(host='localhost', port=6379, db=0)
-with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'diningImages.csv')) as f:
-    reader = csv.DictReader(f)
-    for row in reader:
-        db.set("venue:%s" % (row["id"]), json.dumps(row["imageUrl"]))
+# db = redis.StrictRedis(host='localhost', port=6379, db=0)
+# with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'diningImages.csv')) as f:
+#     reader = csv.DictReader(f)
+#     for row in reader:
+#         db.set("venue:%s" % (row["id"]), json.dumps(row["imageUrl"]))
