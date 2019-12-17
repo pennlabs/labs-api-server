e78d10bd7b407e6cabbd5fa701a5753b603cc1db
Josh <jsdoman97@gmail.com>
2019-12-17 14:56:04 -0500

    handle no assignment

--- server/auth.py
+++ server/auth.py
@@ -9,6 +9,7 @@ def auth():
         @wraps(f)
         def __auth(*args, **kwargs):
             authorization = request.headers.get('Authorization')
+            print(authorization)
             if authorization and ' ' in authorization:
                 auth_type, token = authorization.split()
                 if auth_type == 'Bearer':  # Only validate if Authorization header type is Bearer
@@ -25,13 +26,13 @@ def auth():
                             account = Account.query.filter_by(pennid=data['user']['pennid']).first()
                             return f(account)
                         else:
-                            return jsonify({'error': 'Invalid token'}), 400
+                            return jsonify({'error': 'Invalid token'}), 401
                     except requests.exceptions.RequestException:  # Can't connect to platform
                         # Throw a 403 because we can't verify the incoming access token so we
                         # treat it as invalid. Ideally platform will never go down, so this
                         # should never happen.
-                        return jsonify({'error': 'Unable to connect to Platform'}), 403
+                        return jsonify({'error': 'Unable to connect to Platform'}), 401
             else:
-                return jsonify({'error': 'An access token was not provided.'}), 400
+                return jsonify({'error': 'An access token was not provided.'}), 401
         return __auth
     return _auth
--- server/housing.py
+++ server/housing.py
@@ -13,9 +13,11 @@ class Housing(sqldb.Model):
     house = sqldb.Column(sqldb.Text, nullable=True)
     location = sqldb.Column(sqldb.Text, nullable=True)
     address = sqldb.Column(sqldb.Text, nullable=True)
-    start = sqldb.Column(sqldb.Integer, nullable=False, primary_key=True)
-    end = sqldb.Column(sqldb.Integer, nullable=False)
+    off_campus = sqldb.Column(sqldb.Boolean, nullable=True)
+    start = sqldb.Column(sqldb.Integer, primary_key=True, default=-1)
+    end = sqldb.Column(sqldb.Integer, default=-1)
     created_at = sqldb.Column(sqldb.DateTime, server_default=sqldb.func.now())
+    html = sqldb.Column(sqldb.Text, nullable=True)
 
 
 @app.route('/housing', methods=['POST'])
@@ -24,70 +26,98 @@ def save_housing_info(account):
     html = request.form.get('html')
 
     soup = BeautifulSoup(html, 'html.parser')
-    main = soup.findAll('div', {'class': 'interior-main-content col-md-6 col-md-push-3 md:mb-150'})[0]
+    html = soup.prettify().strip().strip('\t\r\n')
 
-    year_text = None
-    house_text = None
-    headers = main.findAll('h3')
-    for h3 in headers:
-        if 'Academic Year' in h3.text:
-            year_text = h3.text
-        elif 'House Information' in h3.text:
-            house_text = h3.text
+    housing = None
+    try:
+        house, location, address = None, None, None
+        main = soup.findAll('div', {'class': 'interior-main-content col-md-6 col-md-push-3 md:mb-150'})[0]
 
-    info = main.findAll('div', {'class': 'col-md-8'})[0]
-    paragraphs = info.findAll('p')
-    room = paragraphs[0]
-    address = paragraphs[1]
+        off_campus = 'You don\'t have any assignments at this time' in html
+        if off_campus:
+            # Off campus for 2020 - 2021 school year if today is after January and user has no assignments
+            today = datetime.today()
+            start = today.year if today.month > 1 else today.year - 1
+            end = start + 1
+        else:
+            year_text, house_text = None, None
+            headers = main.findAll('h3')
+            for h3 in headers:
+                if 'Academic Year' in h3.text:
+                    year_text = h3.text
+                elif 'House Information' in h3.text:
+                    house_text = h3.text
 
-    split = year_text.strip().split(" ")
-    start, end = split[len(split) - 3], split[len(split) - 1]
+            info = main.findAll('div', {'class': 'col-md-8'})[0]
+            paragraphs = info.findAll('p')
+            room = paragraphs[0]
+            address = paragraphs[1]
 
-    split = house_text.split("-")
-    house = split[1].strip()
+            split = year_text.strip().split(" ")
+            start, end = split[len(split) - 3], split[len(split) - 1]
 
-    split = room.text.split("  ")
-    location = split[0].strip()
+            split = house_text.split("-")
+            house = split[1].strip()
 
-    split = address.text.split("  ")
-    address = split[0].strip()
+            split = room.text.split("  ")
+            location = split[0].strip()
 
-    housing = Housing(account=account.id, house=house, location=location, address=address, start=start, end=end)
+            split = address.text.split("  ")
+            address = split[0].strip()
+
+        housing = Housing(account=account.id, house=house, location=location, address=address, off_campus=off_campus, 
+                            start=start, end=end, html=html)
+    except (IndexError, AttributeError):
+        # Parsing failed. Save the html so that we can diagnose the problem and update the account's info later.
+        housing = Housing(account=account.id, html=html)
+
     try:
         sqldb.session.add(housing)
         sqldb.session.commit()
     except IntegrityError:
         sqldb.session.rollback()
-        current_result = Housing.query.filter_by(account=account.id, start=start).first()
-        if house and location and address and end:
-            current_result.house = house
-            current_result.location = location
-            current_result.address = address
-            current_result.end = end
+        current_result = Housing.query.filter_by(account=account.id, start=housing.start).first()
+        if current_result:
+            if housing.off_campus or (housing.house and housing.location and housing.address):
+                current_result.house = house
+                current_result.location = location
+                current_result.address = address
+                current_result.off_campus = off_campus
+            current_result.html = html
             sqldb.session.commit()
 
-    return jsonify({
-        'house': house,
-        'room': location,
-        'address': address,
-        'start': start,
-        'end': end,
-    })
+    if housing.start:
+        return jsonify({
+            'house': housing.house,
+            'room': housing.location,
+            'address': housing.address,
+            'start': housing.start,
+            'end': housing.end,
+            'off_campus': housing.off_campus,
+        })
+    else:
+        return jsonify({'error': 'Unable to parse HTML.'}), 400
 
 
 @app.route('/housing', methods=['GET'])
 @auth()
 def get_housing_info(account):
     today = datetime.today()
-    year = today.year if today.month > 6 else today.year - 1
+    year = today.year if today.month > 5 else today.year - 1
     housing = Housing.query.filter_by(account=account.id, start=year).first()
-    return jsonify({
-        'house': housing.house,
-        'room': housing.location,
-        'address': housing.address,
-        'start': housing.start,
-        'end': housing.end,
-    })
+    if housing:
+        return jsonify({
+            'result': {
+                'house': housing.house,
+                'room': housing.location,
+                'address': housing.address,
+                'start': housing.start,
+                'end': housing.end,
+                'off_campus': housing.off_campus,
+            }
+        })
+    else:
+        return jsonify({'result': None})
 
 
 def get_details_for_location(location):
