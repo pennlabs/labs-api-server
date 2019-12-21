96999383e18aab1a43ea75712120227d1287e3cf
Josh <jsdoman97@gmail.com>
2019-12-21 18:15:05 -0500

    update cron job command

--- cron/send_gsr_push_notifications.py
+++ cron/send_gsr_push_notifications.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # Add the following line into the labs crontab.
-# */15 * * * * /home/labs/penn-mobile-server/cron/send_gsr_push_notifications.py
+# */20,50 * * * * /home/labs/penn-mobile-server/cron/send_gsr_push_notifications.py
 
 import server
 
