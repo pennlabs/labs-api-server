4409b4a4d2e181acfe3808d7ea378b25ed46b7fc
Josh <jsdoman97@gmail.com>
2019-12-22 11:22:55 -0500

    fixed flake

--- cron/send_gsr_push_notifications.py
+++ cron/send_gsr_push_notifications.py
@@ -2,11 +2,13 @@
 
 # Add the following line into the labs crontab.
 # 20,50 * * * * /home/labs/penn-mobile-server/cron/send_gsr_push_notifications.py
+
 import os
 import sys
 
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 
 if True:
 	import server
