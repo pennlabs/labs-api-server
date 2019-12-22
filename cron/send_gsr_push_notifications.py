7fda670e8db5a8f94cd160da3eeb5b2c2f92d8ea
Josh <jsdoman97@gmail.com>
2019-12-21 23:58:25 -0500

    fixed cronjob send reminders

--- cron/send_gsr_push_notifications.py
+++ cron/send_gsr_push_notifications.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
 # Add the following line into the labs crontab.
-# */20,50 * * * * /home/labs/penn-mobile-server/cron/send_gsr_push_notifications.py
+# 20,50 * * * * /home/labs/penn-mobile-server/cron/send_gsr_push_notifications.py
 
 import server
 
 
-server.studyspaces.send_reminders()
+server.studyspaces.notifications.send_reminders()
