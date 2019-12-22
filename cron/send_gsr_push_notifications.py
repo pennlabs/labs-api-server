8e1b1220e5a2c9cd8b900fd25c86de23d4b9c15e
Josh <jsdoman97@gmail.com>
2019-12-22 00:16:10 -0500

    trest

--- cron/send_gsr_push_notifications.py
+++ cron/send_gsr_push_notifications.py
@@ -2,8 +2,14 @@
 
 # Add the following line into the labs crontab.
 # 20,50 * * * * /home/labs/penn-mobile-server/cron/send_gsr_push_notifications.py
+import os
+import sys
 
-import server
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+if True:
+	import server
 
 
 server.studyspaces.notifications.send_reminders()
