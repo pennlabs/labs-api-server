047897d99fe0e7e802b474db5a8a8680f88a4424
Josh <jsdoman97@gmail.com>
2020-01-18 16:17:13 -0500

    undo analytics

--- server/analytics.py
+++ server/analytics.py
@@ -34,11 +34,11 @@ def send_analytics():
         index = int(event_json.get('index'))
         post_id = event_json.get('id')
         flag = bool(event_json.get('is_interaction'))
-        if any(x == type for x in ['news', 'post']):
+        # if any(x == type for x in ['news', 'post']):
             # Only log news and post events
-            event = AnalyticsEvent(user=user.id, account_id=account_id, timestamp=timestamp, type=type, index=index,
-                                   post_id=post_id, is_interaction=flag)
-            sqldb.session.add(event)
+        event = AnalyticsEvent(user=user.id, account_id=account_id, timestamp=timestamp, type=type, index=index,
+                               post_id=post_id, is_interaction=flag)
+        sqldb.session.add(event)
     sqldb.session.commit()
 
     return jsonify({'success': True, 'error': None})
