96d28cd7f4db7fa0834b4c5fabdb37aa5217cf97
Matthew <matthew@rosca-halmagean.com>
2021-02-11 14:25:21 -0500

    Updated voting to match spec

--- server/polls/vote.py
+++ server/polls/vote.py
@@ -17,27 +17,27 @@ Content-Type: application/json
 Parameters: poll_id, option_id, school, year
 
 Adds new vote
-If successful, returns poll ID
+If successful, returns bool
 """
 @app.route("/api/choosePollOption", methods=["POST"])
 def add_vote():
     data = request.get_json()
 
-    poll = Poll.query.filter_by(id=data.get("poll_id")).first()
-    opt = PollOption.query.filter_by(id=data.get("option_id")).first()
-
-    if not poll:
-        return jsonify({"error": "Poll not found."}), 400
-    if not opt:
-        return jsonify("error": "Poll option not found."), 400
+    if any(x is None for x in [poll_id, option_id, school, year]):
+        return jsonify({"error": "Parameter is missing"}), 400
 
     poll_id = data.get("poll_id")
     choice = data.get("option_id")
     school = data.get("school")
     year = data.get("year")
 
-    if any(x is None for x in [poll_id, option_id, school, year]):
-        return jsonify({"error": "Parameter is missing"}), 400
+    poll = Poll.query.filter_by(id=poll_id).first()
+    opt = PollOption.query.filter_by(id=choice).first()
+
+    if not poll:
+        return jsonify({"error": "Poll not found."}), 400
+    if not opt:
+        return jsonify("error": "Poll option not found."), 400
 
     poll_vote = PollVote(
         poll=poll_id,
@@ -48,4 +48,4 @@ def add_vote():
     sqldb.session.add(poll_vote)
     sqldb.session.commit()
 
-    return jsonify({"poll_id": poll.id})
+    return jsonify({"success": True})
