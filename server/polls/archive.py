79702c71f7e65586e5c8ef2c1f330765c1090b56
Matthew <matthew@rosca-halmagean.com>
2021-02-11 13:54:50 -0500

    Fixed poll info retrieval

--- server/polls/archive.py
+++ server/polls/archive.py
@@ -20,17 +20,20 @@ Returns list of polls
 """
 @app.route("/api/polls", methods=["GET"])
 def get_all_polls():
+    archive = request.args.get("archives")
+
     polls = Poll.query.all()
     polls_query = sqldb.session.query(Poll.id).subquery()
 
     json_arr = []
     for poll in polls:
-        poll_json = get_poll_json(poll)
+        poll_json = get_poll_json(poll, archive)
+        json_arr.append(poll_json)
     
     return jsonify({"polls": json_arr})
 
 
-def get_poll_json(poll):
+def get_poll_json(poll, archive):
     poll_json = {
         "id": poll.id,
         "question": poll.question,
@@ -41,47 +44,54 @@ def get_poll_json(poll):
 
     options = PollOption.query.filter_by(poll=poll.id).all()
     for obj in options:
-        post_json["options"].append({
-            "id": obj.id,
-            "optionText": obj.choice,
-            "votes": PollVote.query.filter_by(choice=obj.id).count(),
-            "votesByYear": [
-                {
-                    "demographic": "2021",
-                    "votes": PollVote.query.filter_by(choice=obj.id, year="2021").count()
-                },
-                {
-                    "demographic": "2022",
-                    "votes": PollVote.query.filter_by(choice=obj.id, year="2022").count()
-                },
-                {
-                    "demographic": "2023",
-                    "votes": PollVote.query.filter_by(choice=obj.id, year="2023").count()
-                },
-                {
-                    "demographic": "2024",
-                    "votes": PollVote.query.filter_by(choice=obj.id, year="2024").count()
-                }
-            ],
-            "votesBySchool": [
-                {
-                    "demographic": "WH",
-                    "votes": PollVote.query.filter_by(choice=obj.id, year="WH").count()
-                },
-                {
-                    "demographic": "COL",
-                    "votes": PollVote.query.filter_by(choice=obj.id, year="COL").count()
-                },
-                {
-                    "demographic": "EAS",
-                    "votes": PollVote.query.filter_by(choice=obj.id, year="EAS").count()
-                },
-                {
-                    "demographic": "NURS",
-                    "votes": PollVote.query.filter_by(choice=obj.id, year="NURS").count()
-                }
-            ]
-        })
+        if archive:
+            poll_json["options"].append({
+                "id": obj.id,
+                "optionText": obj.choice,
+                "votes": PollVote.query.filter_by(choice=obj.id).count(),
+                "votesByYear": [
+                    {
+                        "demographic": "2021",
+                        "votes": PollVote.query.filter_by(choice=obj.id, year="2021").count()
+                    },
+                    {
+                        "demographic": "2022",
+                        "votes": PollVote.query.filter_by(choice=obj.id, year="2022").count()
+                    },
+                    {
+                        "demographic": "2023",
+                        "votes": PollVote.query.filter_by(choice=obj.id, year="2023").count()
+                    },
+                    {
+                        "demographic": "2024",
+                        "votes": PollVote.query.filter_by(choice=obj.id, year="2024").count()
+                    }
+                ],
+                "votesBySchool": [
+                    {
+                        "demographic": "WH",
+                        "votes": PollVote.query.filter_by(choice=obj.id, year="WH").count()
+                    },
+                    {
+                        "demographic": "COL",
+                        "votes": PollVote.query.filter_by(choice=obj.id, year="COL").count()
+                    },
+                    {
+                        "demographic": "EAS",
+                        "votes": PollVote.query.filter_by(choice=obj.id, year="EAS").count()
+                    },
+                    {
+                        "demographic": "NURS",
+                        "votes": PollVote.query.filter_by(choice=obj.id, year="NURS").count()
+                    }
+                ]
+            })
+        else:
+            poll_json["options"].append({
+                "id": obj.id,
+                "optionText": obj.choice,
+                "votes": PollVote.query.filter_by(choice=obj.id).count()
+            })
     
     return poll_json
 
